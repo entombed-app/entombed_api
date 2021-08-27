d40c64247101252006193cfa39859a2121961125
NoahZinter <noah.zinter@gmail.com>
2021-08-27 16:52:52 -0600

    Complete User creation functionality

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user).serializable_hash.to_json, status: 201
+    else
+      render json: {
+        error: 'Missing or incorrect user params', 
+        status: 400
+      }, status: 400
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :date_of_birth, :name, :obituary,:password, :profile_picture)
+  end
+end
