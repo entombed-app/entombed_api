7d5126dde3bf440af065daafd89dcef351510432
NoahZinter <noah.zinter@gmail.com>
2021-08-27 16:58:27 -0600

    Test creation serialization

--- spec/api/v1/requests/users_request_spec.rb
+++ spec/api/v1/requests/users_request_spec.rb
@@ -24,5 +24,44 @@ RSpec.describe 'Users Requests' do
       expect(created_user.password_digest).is_a? String
       expect(created_user.profile_picture).to eq 'www.img-example.com'
     end
+
+    it 'returns serialized user upon creation' do
+      user_details = {
+        email: 'ex@ample.com',
+        date_of_birth: '2001/02/03',
+        name: 'Jane Doe',
+        obituary: 'Tedious and brief',
+        password: 'password',
+        profile_picture: 'www.img-example.com'
+      }
+      headers = {"CONTENT_TYPE"  => 'application/json'}
+      post '/api/v1/users', headers: headers, params: JSON.generate(user_details)
+
+      serialized_user = JSON.parse(response.body, symbolize_names: true)
+
+      expect(serialized_user).is_a? Hash
+      expect(serialized_user).to have_key(:data)
+      expect(serialized_user[:data]).is_a? Hash
+      expect(serialized_user[:data]).to have_key(:id)
+      expect(serialized_user[:data][:id]).is_a? String
+      expect(serialized_user[:data]).to have_key(:type)
+      expect(serialized_user[:data][:type]).to eq "user"
+      expect(serialized_user[:data]).to have_key(:attributes)
+      expect(serialized_user[:data][:attributes]).is_a? Hash
+      expect(serialized_user[:data][:attributes]).not_to have_key(:password)
+      expect(serialized_user[:data][:attributes]).not_to have_key(:password_digest)
+      expect(serialized_user[:data][:attributes]).to have_key(:email)
+      expect(serialized_user[:data][:attributes][:email]).is_a? String
+      expect(serialized_user[:data][:attributes]).to have_key(:name)
+      expect(serialized_user[:data][:attributes][:name]).is_a? String
+      expect(serialized_user[:data][:attributes]).to have_key(:date_of_birth)
+      expect(serialized_user[:data][:attributes][:date_of_birth].to_date).is_a? Date
+      expect(serialized_user[:data][:attributes]).to have_key(:profile_picture)
+      expect(serialized_user[:data][:attributes][:profile_picture]).is_a? String
+      expect(serialized_user[:data][:attributes]).to have_key(:obituary)
+      expect(serialized_user[:data][:attributes][:obituary]).is_a? String
+      expect(serialized_user[:data][:attributes]).to have_key(:etd)
+      expect(serialized_user[:data][:attributes][:etd].to_date).is_a? Date
+    end
   end
 end
