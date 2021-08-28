b5fa481915e28e3b9d888fd13288775854e2b373
NoahZinter <noah.zinter@gmail.com>
2021-08-28 14:06:12 -0600

    Test for mismatched password and confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,20 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-    if user.save
-      render json: UserSerializer.new(user).serializable_hash.to_json, status: 201
-    else
+    if params[:password] != params[:password_confirmation]
       render json: {
-        error: 'Missing or incorrect user params',
-        status: 400
-      }, status: 400
+        error: 'password and confirmation must match', 
+        status: 401
+      }, status: 401
+    else
+      user = User.new(user_params)
+      if user.save
+        render json: UserSerializer.new(user).serializable_hash.to_json, status: 201
+      else
+        render json: {
+          error: 'Missing or incorrect user params',
+          status: 400
+        }, status: 400
+      end
     end
   end
 
