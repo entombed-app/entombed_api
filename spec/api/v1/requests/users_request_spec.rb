f0a2527c54ce37ab71bb965d073edf2d6d9eccb5
NoahZinter <noah.zinter@gmail.com>
2021-08-28 14:19:37 -0600

    Test it creates without profile pic

--- spec/api/v1/requests/users_request_spec.rb
+++ spec/api/v1/requests/users_request_spec.rb
@@ -189,5 +189,28 @@ RSpec.describe 'Users Requests' do
       expect(created_user.password_digest).is_a? String
       expect(created_user.profile_picture).to eq 'www.img-example.com'
     end
+
+    it 'does create without profile pic' do
+      user_details = {
+        email: 'ex@ample.com',
+        date_of_birth: '2001/02/03',
+        name: 'Jane Doe',
+        obituary: 'Tedious and brief',
+        password: 'password',
+        password_confirmation: 'password'
+      }
+      headers = {"CONTENT_TYPE"  => 'application/json'}
+      post '/api/v1/users', headers: headers, params: JSON.generate(user_details)
+      created_user = User.last
+
+      expect(response.status).to eq 201
+      expect(created_user.email).to eq 'ex@ample.com'
+      expect(created_user.date_of_birth.to_s).to eq '2001-02-03'
+      expect(created_user.name).to eq 'Jane Doe'
+      expect(created_user.obituary).to eq 'Tedious and brief'
+      expect(created_user.password).to eq nil
+      expect(created_user.password_digest).is_a? String
+      expect(created_user.profile_picture).to eq nil
+    end
   end
 end
