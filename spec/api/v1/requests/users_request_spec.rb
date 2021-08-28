44507fd10c1c99a9f97709f12ef6d1bf3f11018c
NoahZinter <noah.zinter@gmail.com>
2021-08-28 14:10:15 -0600

    Test it does not create extra data

--- spec/api/v1/requests/users_request_spec.rb
+++ spec/api/v1/requests/users_request_spec.rb
@@ -84,5 +84,33 @@ RSpec.describe 'Users Requests' do
 
       expect(error_message).to eq({ error: 'password and confirmation must match', status: 401 })
     end
+
+    it 'ignores extra data' do
+      user_details = {
+        email: 'ex@ample.com',
+        date_of_birth: '2001/02/03',
+        name: 'Jane Doe',
+        obituary: 'Tedious and brief',
+        password: 'password',
+        password_confirmation: 'password',
+        profile_picture: 'www.img-example.com',
+        foo: 'bar',
+        baz: 'boop'
+      }
+      headers = {"CONTENT_TYPE"  => 'application/json'}
+      post '/api/v1/users', headers: headers, params: JSON.generate(user_details)
+      created_user = User.last
+
+      expect(response.status).to eq 201
+      expect(created_user.email).to eq 'ex@ample.com'
+      expect(created_user.date_of_birth.to_s).to eq '2001-02-03'
+      expect(created_user.name).to eq 'Jane Doe'
+      expect(created_user.obituary).to eq 'Tedious and brief'
+      expect(created_user.password).to eq nil
+      expect(created_user.password_digest).is_a? String
+      expect(created_user.profile_picture).to eq 'www.img-example.com'
+      expect{created_user.foo}.to raise_error(NameError)
+      expect{created_user.baz}.to raise_error(NameError)
+    end
   end
 end
