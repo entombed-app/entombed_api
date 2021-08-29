9c5eac05872647785a2c99986048305cb300bbe5
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-08-29 16:32:19 +0200

    Adds credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, :date_of_birth, presence: true
 
+  has_many :images
+
 
   has_secure_password
 end
