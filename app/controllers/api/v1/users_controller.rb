2b72d2a59840bf831d3c39bc6998750200396808
NoahZinter <noah.zinter@gmail.com>
2021-08-29 10:11:07 -0600

    Remove refs to password_confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,20 +1,13 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    if params[:password] != params[:password_confirmation]
-      render json: {
-        error: 'password and confirmation must match', 
-        status: 401
-      }, status: 401
+    user = User.new(user_params)
+    if user.save
+      render json: UserSerializer.new(user).serializable_hash.to_json, status: 201
     else
-      user = User.new(user_params)
-      if user.save
-        render json: UserSerializer.new(user).serializable_hash.to_json, status: 201
-      else
-        render json: {
-          error: 'Missing or incorrect user params',
-          status: 400
-        }, status: 400
-      end
+      render json: {
+        error: 'Missing or incorrect user params',
+        status: 400
+      }, status: 400
     end
   end
 
