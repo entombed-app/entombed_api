53bf801af6b8b7e6f1ed745e7abe87adb9a067a1
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-08-30 23:54:32 +0200

    tiny commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', 'elegy-backend.herokuapp.com', 'localhost:8080'
+    origins '*'
 
     resource '*',
       headers: :any,
