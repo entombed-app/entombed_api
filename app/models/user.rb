22df9d716e20c7a3f72b72d193f0cb0970bcd171
NoahZinter <noah.zinter@gmail.com>
2021-08-31 11:47:09 -0600

    Add executors table and test relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :name, :date_of_birth, presence: true
 
+  has_many :executors, dependent: :destroy
   has_one_attached :profile_picture, dependent: :destroy
   validates :profile_picture, content_type: [:png, :jpg, :jpeg]
   
