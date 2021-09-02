e66d8ab165c37e3750267bf008d422e40780d98e
NoahZinter <noah.zinter@gmail.com>
2021-09-01 18:01:11 -0600

    Add sample user for validation testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User do
+  before(:all) do
+    create(:user)
+  end
   describe 'validations' do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
