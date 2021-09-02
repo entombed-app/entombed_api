6f997e1e0037e6a714ac7d929feb05284cda1142
NoahZinter <noah.zinter@gmail.com>
2021-09-02 17:45:21 -0600

    Test Executors index

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:create, :show, :update] do
         get :profile_picture, on: :member
-        resources :executors, only: [:create, :update, :destroy]
+        resources :executors, only: [:create, :update, :destroy, :index]
       end 
     end
   end
