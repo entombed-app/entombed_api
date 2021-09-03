d0fcfc24b8879890407fb9ad229a63477357f7d8
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-09-04 01:28:27 +0200

    updates default route to heroku

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+Rails.application.routes.default_url_options[:host] = 'elegy-backend.herokuapp.com'
 
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
