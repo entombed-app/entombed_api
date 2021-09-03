30cdbdd584603d3167e24c36a75281df69c7da12
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-09-04 00:36:48 +0200

    refactor url method into user model

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :email, :name, :date_of_birth, :obituary, :profile_picture
+  attributes :email, :name, :date_of_birth, :obituary, :profile_picture_url
 
   has_many :executors, serializer: ExecutorSerializer
 
@@ -8,9 +8,5 @@ class UserSerializer
     lived_days = (Date.today - object.date_of_birth).to_i
     remaining = ( 44000 - lived_days ).to_i
     max_date = ( Date.today + remaining )
-  end
-
-  attribute :profile_picture do |object| 
-    Rails.application.routes.url_helpers.rails_blob_url(object.profile_picture) if object.profile_picture.attached?
   end
 end
