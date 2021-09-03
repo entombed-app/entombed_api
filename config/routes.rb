c9f2f89b78f73acf8653f2347feeb1edbcbe9e96
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-09-04 00:29:28 +0200

    updates routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :show, :update] do
+        patch '/profile_picture', to: 'users#profile_picture'
         get :profile_picture, on: :member
         resources :executors, only: [:create, :update, :destroy, :index]
       end 
