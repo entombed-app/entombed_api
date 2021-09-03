236308e2adbf504ec25ada5cf65e93a3bf60ddd1
NoahZinter <noah.zinter@gmail.com>
2021-09-03 17:52:32 -0600

    Test happy path session creation

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,5 +1,12 @@
-class Api::V1:SessionsController < ApplicationController
+class Api::V1::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email], password: params[:password])
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      render json: UserSerializer.new(user)
+    else
+      render json: {
+        error: 'Username or password incorrect'
+      }, status: 403
+    end
   end
 end
