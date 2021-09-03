ea5eaad925daa55cc5770d8c063845faabfc5157
NoahZinter <noah.zinter@gmail.com>
2021-09-03 17:14:45 -0600

    Set up routes and testing for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get '/login', to: 'sessions#create'
       resources :users, only: [:create, :show, :update] do
         patch '/profile_picture', to: 'users#profile_picture'
         get :profile_picture, on: :member
         resources :executors, only: [:create, :update, :destroy, :index]
-      end 
+      end
     end
   end
 end
