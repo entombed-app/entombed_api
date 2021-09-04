4606685d4738a94f0aa22aeb2df91cc023782ac0
Noah Zinter <77814101+NoahZinter@users.noreply.github.com>
2021-09-04 11:57:24 -0600

    Revert "Test sad path session creation"
    This reverts commit 293f127bf2d9171d2c5f920a7bb6d1e8699c2f7e.

--- spec/api/v1/requests/sessions_request_spec.rb
+++ spec/api/v1/requests/sessions_request_spec.rb
@@ -45,53 +45,5 @@ RSpec.describe 'Sessions Requests' do
       expect(found_user[:data][:attributes]).to have_key(:etd)
       expect(found_user[:data][:attributes][:etd].to_date).is_a? Date
     end
-
-    it 'returns an error for non existent users' do
-      user_details = {
-        email: 'ex@ample.com',
-        date_of_birth: '2001/02/03',
-        name: 'Jane Doe',
-        obituary: 'Tedious and brief',
-        password: 'password123'
-      }
-      headers = {"CONTENT_TYPE"  => 'application/json'}
-      post '/api/v1/users', headers: headers, params: JSON.generate(user_details)
-      created_user = User.last
-
-      find_user = {
-        email: 'foo',
-        password: 'bar'
-      }
-      post '/api/v1/login', headers: headers, params: JSON.generate(find_user)
-
-      expect(response.status).to eq 403
-      
-      error_message = JSON.parse(response.body, symbolize_names: true)
-      expect(error_message[:error]).to eq 'Username or password incorrect'
-    end
-
-    it 'returns the same error for incorrect password' do
-      user_details = {
-        email: 'ex@ample.com',
-        date_of_birth: '2001/02/03',
-        name: 'Jane Doe',
-        obituary: 'Tedious and brief',
-        password: 'password123'
-      }
-      headers = {"CONTENT_TYPE"  => 'application/json'}
-      post '/api/v1/users', headers: headers, params: JSON.generate(user_details)
-      created_user = User.last
-
-      find_user = {
-        email: 'ex@ample.com',
-        password: 'bar'
-      }
-      post '/api/v1/login', headers: headers, params: JSON.generate(find_user)
-
-      expect(response.status).to eq 403
-      
-      error_message = JSON.parse(response.body, symbolize_names: true)
-      expect(error_message[:error]).to eq 'Username or password incorrect'
-    end
   end
 end
