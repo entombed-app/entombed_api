693800c925f3689d92a4e891c6e7f29fe86495d5
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-09-06 00:01:58 +0200

    adds email function to user

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,15 @@
+class UserMailer < ApplicationMailer
+    default :from => 'elegy.notify@gmail.com'
+
+  # send a signup email to the user, pass in the user object that   contains the user's email address
+  def send_email(user)
+    user.recipients.each do |recipient|
+        @recipient = recipient
+        @user = user
+        mail( 
+            :to => @recipient.email,
+            :subject => 'Our Condolences'
+        )
+    end
+  end
+end
