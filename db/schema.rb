d3a7179a7f7b4a4ec10e9a32edf98468913db486
Jaybraum <76916134+Jaybraum@users.noreply.github.com>
2021-09-05 18:04:54 +0200

    adds recipients to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_233805) do
+ActiveRecord::Schema.define(version: 2021_09_05_150747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,15 @@ ActiveRecord::Schema.define(version: 2021_09_01_233805) do
     t.index ["user_id"], name: "index_executors_on_user_id"
   end
 
+  create_table "recipients", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_recipients_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "password_digest"
     t.string "email", null: false
@@ -58,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_09_01_233805) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "executors", "users"
+  add_foreign_key "recipients", "users"
 end
