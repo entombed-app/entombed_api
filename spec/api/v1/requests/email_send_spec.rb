0ab9d70b6e0d6b2983c254b9286bd2d8a9d52995
NoahZinter <noah.zinter@gmail.com>
2021-09-06 13:02:45 -0600

    Test for error handling in emails

--- spec/api/v1/requests/email_send_spec.rb
+++ spec/api/v1/requests/email_send_spec.rb
@@ -22,26 +22,32 @@ RSpec.describe 'Email send' do
     end
 
     it 'can send emails to multiple user recipients' do
-        user = create(:user)
-        user.profile_picture.attach(
-              io: File.open('./public/profile_pictures/profile-picture.png'),
-              filename: 'profile-picture.png',
-              content_type: 'application/png'
-          )
-        user.recipients.create!(name: 'Uncle bobby', email: 'unclebobby@test.com')
-        user.recipients.create!(name: 'Aunty bobby', email: 'auntybobby@test.com')
-        user.recipients.create!(name: 'Gary bobby', email: 'garybobby@test.com')
-        user.recipients.create!(name: 'Jerry bobby', email: 'Jerrybobby@test.com')
-        user.recipients.create!(name: 'Cousin bobby', email: 'Cousinbobby@test.com')
-  
-        post "/api/v1/users/#{user.id}/email"
-  
-        email = UserMailer.send_email(user)
-  
-        expect(email.subject).to eq("Our Condolences")
-        expect(email.to[0]).to be_a(String)
-        expect(email.from[0]).to be_a(String)
-        expect(email.body).to include("Sorry to inform you,")
+      user = create(:user)
+      user.profile_picture.attach(
+            io: File.open('./public/profile_pictures/profile-picture.png'),
+            filename: 'profile-picture.png',
+            content_type: 'application/png'
+        )
+      user.recipients.create!(name: 'Uncle bobby', email: 'unclebobby@test.com')
+      user.recipients.create!(name: 'Aunty bobby', email: 'auntybobby@test.com')
+      user.recipients.create!(name: 'Gary bobby', email: 'garybobby@test.com')
+      user.recipients.create!(name: 'Jerry bobby', email: 'Jerrybobby@test.com')
+      user.recipients.create!(name: 'Cousin bobby', email: 'Cousinbobby@test.com')
+
+      post "/api/v1/users/#{user.id}/email"
+
+      email = UserMailer.send_email(user)
+
+      expect(email.subject).to eq("Our Condolences")
+      expect(email.to[0]).to be_a(String)
+      expect(email.from[0]).to be_a(String)
+      expect(email.body).to include("Sorry to inform you,")
+    end
+
+    it 'will return an errorr if user not found' do
+      expect {
+        post "/api/v1/users/8000/email"
+      }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
